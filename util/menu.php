20c55f092a64bf59f110af1650bc20e38743c683
Bas <bas@basbroek.nl>
2015-06-09 14:54:26 +0200

    Using PHP to prevent repeating of code

---
+++ util/menu.php
@@ -0,0 +1,11 @@
+<?php
+echo '<nav>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="onderzoeken.php">Onderzoeken</a></li>
+    <li><a href="#">Architectuur</a></li>
+    <li><a href="#">Documentatie</a></li>
+  </ul>
+</nav>
+<section class="main-content">';
+?>

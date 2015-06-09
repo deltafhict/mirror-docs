62458ed5f3d6086da96c954bcf649afab6b676f9
Tim Castelijns <timcastelijns@gmail.com>
2015-06-09 14:55:40 +0200

    Include project and product documentation

--- util/menu.php
+++ util/menu.php
@@ -4,7 +4,14 @@ echo '<nav>
     <li><a href="index.php">Home</a></li>
     <li><a href="onderzoeken.php">Onderzoeken</a></li>
     <li><a href="#">Architectuur</a></li>
-    <li><a href="#">Documentatie</a></li>
+
+    </li>
+  </ul>
+  <br />
+  <b>Documentatie</b>
+  <ul>
+    <li><a href="project_documentatie.php">Project</a></li>
+    <li><a href="product_documentatie.php">Product</a></li>
   </ul>
 </nav>
 <section class="main-content">';

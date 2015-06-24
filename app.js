94490066fe0f62885044b627fb12110f6061e34d
Tim Castelijns <timcastelijns@gmail.com>
2015-06-24 10:13:52 +0200

    Applied some structure to the routing table

--- app.js
+++ app.js
@@ -4,54 +4,17 @@ var app = angular.module('mirror-docs', [
 
 app.config(function($routeProvider) {
 	$routeProvider
+
+	/*
+	 * Home
+	 */
 	.when('/', {
 		templateUrl: 'pages/home.html'
 	})
-	.when('/kinect/waarom', {
-		templateUrl: 'pages/kinect/waarom.html'
-	})
-	.when('/kinect/hardware-setup', {
-		templateUrl: 'pages/kinect/hardware_setup.html'
-	})
-	.when('/kinect/bevindingen', {
-		templateUrl: 'pages/kinect/bevindingen.html'
-	})
-	.when('/kinect/gestures', {
-		templateUrl: 'pages/kinect/gestures.html'
-	})
-	.when('/kinect/voice', {
-		templateUrl: 'pages/kinect/voice.html'
-	})
-	.when('/leap/waarom', {
-		templateUrl: 'pages/leapmotion/waarom.html'
-	})
-	.when('/leap/hoewerkthet', {
-		templateUrl: 'pages/leapmotion/hoe_werkt_het.html'
-	})
-	.when('/leap/bevindingen', {
-		templateUrl: 'pages/leapmotion/bevindingen.html'
-	})
-	.when('/concepts/clothing', {
-		templateUrl: 'pages/clothing.html'
-	})
-	.when('/concepts/fitness', {
-		templateUrl: 'pages/fitness.html'
-	})
-	.when('/concepts/good-morning', {
-		templateUrl: 'pages/good_morning.html'
-	})
-	.when('/concepts/hub', {
-		templateUrl: 'pages/hub.html'
-	})
-	.when('/concepts/kapper', {
-		templateUrl: 'pages/kapper.html'
-	})
-	.when('/docs/product', {
-		templateUrl: 'pages/product.html'
-	})
-	.when('/docs/project', {
-		templateUrl: 'pages/project.html'
-	})
+
+	/*
+	 * Onderzoeken
+	 */
 	.when('/research/facerecognition', {
 		templateUrl: 'pages/research/facerecognition.html'
 	})
@@ -73,6 +36,71 @@ app.config(function($routeProvider) {
 	.when('/research/gestures-kinect', {
 		templateUrl: 'pages/research/gestures-kinect.html'
 	})
+
+	/*
+	 * Documentatie
+	 */
+	.when('/docs/product', {
+		templateUrl: 'pages/product.html'
+	})
+	.when('/docs/project', {
+		templateUrl: 'pages/project.html'
+	})
+
+	/*
+	 * Concepten
+	 */
+	.when('/concepts/clothing', {
+		templateUrl: 'pages/clothing.html'
+	})
+	.when('/concepts/fitness', {
+		templateUrl: 'pages/fitness.html'
+	})
+	.when('/concepts/good-morning', {
+		templateUrl: 'pages/good_morning.html'
+	})
+	.when('/concepts/hub', {
+		templateUrl: 'pages/hub.html'
+	})
+	.when('/concepts/kapper', {
+		templateUrl: 'pages/kapper.html'
+	})
+
+	/*
+	 * Kinect
+	 */
+	.when('/kinect/waarom', {
+		templateUrl: 'pages/kinect/waarom.html'
+	})
+	.when('/kinect/hardware-setup', {
+		templateUrl: 'pages/kinect/hardware_setup.html'
+	})
+	.when('/kinect/bevindingen', {
+		templateUrl: 'pages/kinect/bevindingen.html'
+	})
+	.when('/kinect/gestures', {
+		templateUrl: 'pages/kinect/gestures.html'
+	})
+	.when('/kinect/voice', {
+		templateUrl: 'pages/kinect/voice.html'
+	})
+
+	/*
+	 * Leap Motion
+	 */
+	.when('/leap/waarom', {
+		templateUrl: 'pages/leapmotion/waarom.html'
+	})
+	.when('/leap/hoewerkthet', {
+		templateUrl: 'pages/leapmotion/hoe_werkt_het.html'
+	})
+	.when('/leap/bevindingen', {
+		templateUrl: 'pages/leapmotion/bevindingen.html'
+	})
+
+	/*
+	 * UI/UX
+	 */
 	.when('/uiux/testopzet', {
 		templateUrl: 'pages/uiux/testopzet.html'
 	})
